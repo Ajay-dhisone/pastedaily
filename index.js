fa6efcaec71ca49ff00208ff8ee2e8fa9ec5860f
Ajay-dhisone <ajdhisone@gmail.com>
2024-01-24 16:57:50 +0000

    new update style ejs

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
+const ejs = require("ejs")
+
+
+app.set('views', './public');
+app.set('view engine', 'ejs');
+// app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.send("<h1>i am good </h1>")
+    res.render("home.ejs")
+    
 })
 
 app.listen(8000,()=>{console.log(1);})
